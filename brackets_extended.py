38c4f4a961ae672cfdad3789c911dbc0154e724e
jerryntom <84386484+jerryntom@users.noreply.github.com>
2024-02-16 16:18:02 +0100

    Update brackets_extended.py

--- brackets_extended.py
+++ brackets_extended.py
@@ -1,65 +1,74 @@
-class Stack:
+class Stos:
     def __init__(self):  # Konstruktor
+
         self.items = []
 
     def push(self, item):  # Dodawanie elementu
+
         self.items.append(item)
 
     def pop(self):  # Usuwanie elementu
+
         if self.is_empty():
             return None
 
         return self.items.pop(len(self.items) - 1)
 
     def peek(self):
+
         if self.is_empty():
             return None
 
         return self.items.pop(0)
 
     def size(self):
+
         return len(self.items)
 
     def clear(self):
+
         self.items = []
 
     def is_empty(self):
+
         if len(self.items) == 0:
+
             return 1
 
         else:
+
             return 0
 
     def print_stack(self):
+
         for elem in self.items:
             print(elem, end=' ')
 
         print()
 
 
-def check_brackets_ext(seq):
-    brackets = Stack()
-    brackets_reversed = {'(': ')', '[': ']', '{': '}'}
+def check_brackets(data_string):
+    nawiasy = Stos()
 
-    if len(seq) == 0:
+    if len(data_string) == 0:
         return False
 
-    for char in seq:
-        if char in '([{':
-            brackets.push(znak)
-        elif char in ')]}' and not brackets.is_empty():
-            if char == brackets_reversed[brackets.items[len(brackets.items) - 1]]:
-                brackets.pop()
+    for elem in data_string:
+        if elem == '(':
+            nawiasy.push(elem)
+        elif elem == ')' and not nawiasy.is_empty():
+            nawiasy.pop()
         else:
-            brackets.clear()
+            nawiasy.clear()
             return False
 
-    if brackets.is_empty():
+    if nawiasy.is_empty():
         return True
+
     else:
-        brackets.clear()
+        nawiasy.clear()
         return False
 
 
-data = input("Wprowadz ciag okraglych, kwadratowych i klamrowych nawiasow:")
-print(check_brackets_ext(data))
+dane = input("Wprowadz ciag okraglych nawiasow:")
+print(check_brackets(dane))
